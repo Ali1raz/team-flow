e145514452f363aae68ff1a82588e5b3aa25d01c
Ali Raza <alirazakhalid1129@gmail.com>
2026-06-28 20:49:38 +0500

    bugfix: use session on client side, remove orpc handler for session

--- app/(workspace)/workspaces/_components/header.tsx
+++ app/(workspace)/workspaces/_components/header.tsx
@@ -4,28 +4,35 @@ import Image from "next/image";
 import Logo from "@/public/team-flow.png";
 import { ThemeToggle } from "@/components/general/theme-toggle";
 import UserAvatarDropdown from "@/components/general/user-avatar-dropdown";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { orpc } from "@/lib/orpc";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
 
 export function WorkspaceHeader() {
-  const {
-    data: { user },
-  } = useSuspenseQuery(orpc.workspace.list.queryOptions());
+  const { data: session, isPending } = authClient.useSession();
+  const user = session?.user;
 
   return (
     <header className="w-full fixed top-0 left-0 px-4 py-4 bg-muted/50 backdrop-blur-sm border-b z-50">
       <div className="flex items-center max-w-6xl mx-auto justify-between">
-        <div className="flex items-center gap-2">
-          <Image src={Logo} alt="Logo" width={40} height={40} /> TeamFlow
-        </div>
+        <Link href="/">
+          <div className="flex items-center gap-2">
+            <Image src={Logo} alt="Logo" width={40} height={40} /> TeamFlow
+          </div>
+        </Link>
 
         <div className="flex items-center gap-2">
           <ThemeToggle />
-          <UserAvatarDropdown
-            email={user.email}
-            image={user.image}
-            name={user.name}
-          />
+          {isPending ? (
+            <div className="rounded-full w-16 h-8 bg-muted/90" />
+          ) : (
+            user && (
+              <UserAvatarDropdown
+                email={user.email}
+                image={user.image}
+                name={user.name}
+              />
+            )
+          )}
         </div>
       </div>
     </header>
